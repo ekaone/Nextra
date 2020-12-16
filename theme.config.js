045acbc5af866f6e3498036d2f8b1cda509b62c9
ekaone <ekaone3033@gmail.com>
2020-12-16 19:50:25 +0700

    feat: add properties config

--- theme.config.js
+++ theme.config.js
@@ -3,9 +3,9 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Eka Nextra</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next Site Builder
+        Eka The Next Site Builder
       </span>
     </>
   ),
@@ -31,5 +31,8 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
+  repository: 'https://github.com/ekaone/Nextra',
+  docsRepository: 'https://github.com/ekaone/Nextra',
+  branch: 'main',
   footerText: <>MIT {new Date().getFullYear()} © Ekaone.</>,
 }
